ac211a6891608d9192fc1afc3c74c83525c60b51
Igor <kisel.igor@yahoo.com>
2023-10-04 16:21:08 +0300

    pics err fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import multer from 'multer'
 import cors from 'cors'
+import fs from 'fs'
 
 import {
     registerValidation,
@@ -27,6 +28,9 @@ const app = express()
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if(!fs.existsSync('uploads')){
+            fs.mkdirSync('uploads')
+        }
         cb(null, 'uploads')
     },
     filename: (_, file, cb) => {
